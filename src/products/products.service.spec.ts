fde26dd44096f874d948a6a8478573cda1e5d4c1
MartinC <59508264+martinc1991@users.noreply.github.com>
2024-04-26 19:03:51 -0300

    Mocked prisma

--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { ProductsService } from './products.service';
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  let prismaMock: DeepMockProxy<PrismaClient>;
 
   beforeEach(async () => {
+    // https://medium.com/@bonaventuragal/nestjs-testing-recipe-mocking-prisma-274c212d4b80
+    prismaMock = mockDeep<PrismaClient>();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        {
+          provide: PrismaService,
+          useValue: prismaMock,
+        },
+      ],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
@@ -14,5 +27,17 @@ describe('ProductsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  // TODO: Seguir aca, escribir los tests
+
+  it('should return user if exists', async () => {
+    const existingProduct = {
+      username: 'existing-product',
+      name: 'Existing product',
+    };
+
+    await service.findOne(existingProduct.username);
+    expect(prismaMock.products.findUniqueOrThrow).toHaveBeenCalledTimes(1);
   });
 });
