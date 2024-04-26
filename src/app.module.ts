8e052088e987fad1d3398943d4d82643edaa6cc2
MartinC <59508264+martinc1991@users.noreply.github.com>
2024-04-26 19:03:00 -0300

    Remove unused service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { RestaurantsModule } from './restaurants/restaurants.module';
-import { ProductsModule } from './products/products.module';
 import { MenusModule } from './menus/menus.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [PrismaModule, RestaurantsModule, ProductsModule, MenusModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
 })
 export class AppModule {}
