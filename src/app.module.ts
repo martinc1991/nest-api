d33fb1061cbce77cea280d8a59807034077b0cfe
MartinC <59508264+martinc1991@users.noreply.github.com>
2024-04-26 12:43:07 -0300

    Add menus

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { ProductsModule } from './products/products.module';
+import { MenusModule } from './menus/menus.module';
 
 @Module({
-  imports: [PrismaModule, RestaurantsModule, ProductsModule],
+  imports: [PrismaModule, RestaurantsModule, ProductsModule, MenusModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
