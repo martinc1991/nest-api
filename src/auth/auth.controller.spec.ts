15fafdc16cf506136249e23eda28696b1bed4380
MartinC <59508264+martinc1991@users.noreply.github.com>
2024-04-30 13:55:48 -0300

    All tests passing

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let prismaMock: DeepMockProxy<PrismaClient>;
+  let serviceMock: DeepMockProxy<AuthService>;
 
   beforeEach(async () => {
+    prismaMock = mockDeep<PrismaClient>();
+    serviceMock = mockDeep<AuthService>();
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        { provide: AuthService, useValue: serviceMock },
+        { provide: PrismaService, useValue: prismaMock },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
