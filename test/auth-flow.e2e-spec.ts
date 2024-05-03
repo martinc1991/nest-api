78726745a295e558247574031123dd5bb2cacc86
MartinC <59508264+martinc1991@users.noreply.github.com>
2024-05-03 09:54:29 -0300

    Auth flow tested

--- test/auth-flow.e2e-spec.ts
+++ test/auth-flow.e2e-spec.ts
@@ -11,6 +11,7 @@ const userOne: AuthDto = { email: 'nLsDv@example.com', password: '1234567' };
 const invalidUser = { email: 'not-an-email', password: '123' };
 
 const SIGNUP_ROUTE = '/auth/signup';
+const SIGNIN_ROUTE = '/auth/signin';
 
 describe('Auth flow', () => {
   let app: INestApplication;
@@ -36,7 +37,7 @@ describe('Auth flow', () => {
     prisma = moduleFixture.get<PrismaService>(PrismaService);
   });
 
-  describe.only('User signup', () => {
+  describe('User signup', () => {
     beforeEach(async () => {
       await prisma.user.deleteMany();
     });
@@ -72,7 +73,7 @@ describe('Auth flow', () => {
 
       expect(status).toBe(201);
     });
-    it.only('A user should not be able to sign up if email is already taken', async () => {
+    it('A user should not be able to sign up if email is already taken', async () => {
       const user = await request(app.getHttpServer())
         .post(SIGNUP_ROUTE)
         .send(userOne);
@@ -89,10 +90,45 @@ describe('Auth flow', () => {
     });
   });
 
-  describe.only('User signin', () => {
-    it('A user should not be able to signin, if invalid email is provided', async () => {});
-    it('A user should not be able to signin, if invalid password is provided', async () => {});
-    it('A user should not be able to signin, if valid email is provided but password is incorrect', async () => {});
-    it('A user should be able to signin, if valid email and password are provided and they match', async () => {});
+  describe('User signin', () => {
+    beforeAll(async () => {
+      await prisma.user.deleteMany();
+      await request(app.getHttpServer()).post(SIGNUP_ROUTE).send(userOne);
+    });
+
+    it('A user should not be able to signin, if invalid email or password are provided', async () => {
+      const invalidEmailReq = await request(app.getHttpServer())
+        .post(SIGNIN_ROUTE)
+        .send({ ...userOne, email: invalidUser.email });
+      const invalidPasswordReq = await request(app.getHttpServer())
+        .post(SIGNIN_ROUTE)
+        .send({ ...userOne, password: invalidUser.password });
+
+      expect(invalidEmailReq.status).toBe(400);
+      expect(invalidPasswordReq.status).toBe(400);
+    });
+    it('A user should not be able to signin, if valid email and password are provided but password doesnt match', async () => {
+      const invalidPasswordReq = await request(app.getHttpServer())
+        .post(SIGNIN_ROUTE)
+        .send({ ...userOne, password: 'invalid-password' });
+
+      expect(invalidPasswordReq.status).toBe(401);
+    });
+    it('A user should be able to signin, if valid email and password are provided and they match', async () => {
+      const validReq = await request(app.getHttpServer())
+        .post(SIGNIN_ROUTE)
+        .send(userOne);
+
+      expect(validReq.status).toBe(200);
+    });
+    it('A user that successfully signs in should get an access token and a refresh token', async () => {
+      const validReq = await request(app.getHttpServer())
+        .post(SIGNIN_ROUTE)
+        .send(userOne);
+
+      expect(validReq.status).toBe(200);
+      expect(validReq.body.access_token).toBeDefined();
+      expect(validReq.body.refresh_token).toBeDefined();
+    });
   });
 });
